d3b2ecebe4f67e95e26667a2824ba83dbb6ca4a6
morouche <n.morouche@gmail.com>
2023-08-18 11:53:44 +0200

    ajout_agence

---
+++ agence_bancaire/agence.php
@@ -0,0 +1,16 @@
+<?php
+
+class AgenceBancaire{
+    private $name;
+    private $adress;
+    private $account = array();
+
+    public function __construct($nom , $adresse){
+        $this ->nom = $nom;
+        $this ->adresse =$adresse;
+    }
+        
+    }
+
+}
+
