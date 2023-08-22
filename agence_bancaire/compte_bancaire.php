623a0b641647e9d95ab868cfb9ec4d5b3f56f4f4
morouche <n.morouche@gmail.com>
2023-08-22 16:28:59 +0200

    mise a jour compte bancaire

---
+++ agence_bancaire/compte_bancaire.php
@@ -0,0 +1,56 @@
+<?php
+class BankAccount{
+    private $solde;
+    private $numerocompte;
+    private $nomTitulaire;
+    private $typeCompte;
+
+    public function __construct($solde, $numerocompte, $nomTitulaire, $typeCompte) {
+        $this-> solde =$solde ;
+        $this-> numerocompte=$numerocompte;
+        $this-> nomTitulaire=$nomTitulaire;
+        $this-> typeCompte=$typeCompte;
+    
+        
+}
+
+    public function depot($montant){
+        if($montant>0){
+        $this->solde+=$montant;
+        return "Déposé:$". number_format($montant,2)."\n";
+    }else{
+        return "montant invalide\n";
+
+    }
+}
+
+public function retrait($montant){
+    if ($montant > 0 && $montant <=$this-> solde){
+        $this->solde-=$montant;
+        return"retiré:$" .number_format($montant,2)."\n";
+    }else{
+        return "Montant invalide ou solde insuffisant\n";
+    }
+
+}
+
+        public function getSolde() {
+                return "solde: $" . number_format($this->solde,2)."\n";
+
+        }
+
+        public function getNumerocompte(){
+                return"numerocompte: $". number_format
+        }
+
+
+        public function getNomTitulaire(){
+            return "nomTitulaire: $". number_format($this->nomTitulaire."\n");
+
+        }
+        public function getTypeCompte(){
+            return "typeCompte: $". number_format($this->typeCompte."\n");
+        }
+    }
+
+    $moncompte = new BankAccount(1000,123456780,'johnny','courant');
